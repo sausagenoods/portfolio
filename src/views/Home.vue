81826d816b847bc94f869f3ce79500fd6af8a058
Siren <siren@kernal.eu>
2024-10-20 03:23:10 +0300

    Spice it up and use webp images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,10 @@ import { ref, onMounted } from 'vue'
 import { GitHubIcon, MatrixIcon, LinkedInIcon } from 'vue3-simple-icons'
 import ProjectCard from '../components/ProjectCard.vue'
 import BlogPosts from '../components/BlogPosts.vue'
+import { Vue3Marquee } from 'vue3-marquee'
+import { useWindowSize, useColorMode } from '@vueuse/core'
+import { SunIcon, MoonIcon } from '@heroicons/vue/24/solid'
 
-const fontClass = ref('font-sans')
-const fonts = ['font-sans', 'font-mono', 'italic', 'font-extrabold', 'font-light']
 const posts = [
     { title: 'How Chainalysis Made Their Way into Popular Monero Wallets', url: 'https://www.digilol.net/blog/chainanalysis-malicious-xmr.html' },
     { title: 'Establishing persistence using extended attributes on Linux', url: 'https://kernal.eu/posts/linux-xattr-persistence' },
@@ -14,37 +15,71 @@ const posts = [
     { title: 'How to Make a Responsive Contact Form Using Vue.js and Go', url: 'https://www.digilol.net/blog/contact-form-in-vuejs.html' },
 ]
 
-onMounted(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-        fontClass.value = fonts[index]
-        index = (index + 1) % fonts.length
-    }, 500)
+const helloArray = ['Hello!\xa0', 'Labas!\xa0', 'Merhaba!\xa0']
+const cyaArray = ['See you!\xa0']
+const { width } = useWindowSize()
+const isMobile = ref(false)
+const colorMode = useColorMode()
 
-    setTimeout(() => {
-        clearInterval(interval)
-    }, 5000)
+const toggleTheme = () => {
+    colorMode.value = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+
+onMounted(() => {
+    isMobile.value = width.value <= 768
 })
 </script>
 
 <template>
-    <div class="relative flex flex-row min-h-screen justify-center items-center px-5 overflow-hidden">
-        <div class="main-content">
-            <h1 :class="['text-4xl md:text-5xl mb-5', fontClass]">Hello!</h1>
-            <h2 class="text-2xl md:text-3xl mb-5">My name is İrem. Here are some quick facts about me:</h2>
-            <ul class="list-inside list-['-_']">
-                <li class="mb-2 text-xl md:text-2xl">I'm mainly a Go and Vue.js/TypeScript developer.</li>
-                <li class="mb-2 text-xl md:text-2xl">I system-administer an awesome community called <a
-                        href="https://kernal.eu" class="italic underline">kernal.eu</a>.</li>
-                <li class="mb-2 text-xl md:text-2xl">I'm a <a class="italic underline"
-                        href="https://kernal.eu/~siren/pentestplus.pdf">certified</a> penetration tester.</li>
-                <li class="mb-2 text-xl md:text-2xl">I'm a free software advocate and a Linux user of ~10 years.
-                    Currently on <a href="https://github.com/sausagenoods/nixconfig" class="underline">NixOS</a>.</li>
-                <li class="mb-2 text-xl md:text-2xl">I'm one of the directors of a <a href="https://twed.org"
-                        class="italic underline">non-profit</a> in Czech Republic whose main activity is organizing <a
-                        href="https://monerokon.org" class="italic underline">Monero Konferenco</a>.</li>
-                <li class="mb-2 text-xl md:text-2xl">I'm a 4th year informatics student at VILNIUS TECH.</li>
-            </ul>
+    <div class="absolute top-4 md:top-6 left-4 md:left-6 ">
+        <button @click="toggleTheme"
+            class="text-xl bg-transparent hover:bg-black dark:hover:bg-white text-black dark:text-white font-semibold hover:text-white dark:hover:text-black p-2 border border-black dark:border-white hover:border-transparent rounded-full transition-colors">
+            <span class="w-32 h-12" v-if="colorMode === 'light'">
+                <SunIcon class="w-6 h-6 m-1" />
+            </span>
+            <span v-else>
+                <MoonIcon class="w-6 h-6 m-1" />
+            </span>
+        </button>
+    </div>
+
+    <Vue3Marquee class="led md:mt-12 mt-2 overflow-hidden" :clone="true" :gradient="isMobile"
+        :gradient-color="colorMode === 'light' ? [255, 255, 255] : [0, 0, 0]">
+        <span v-for="(word, index) in helloArray" :key="index">
+            {{ word }}
+        </span>
+    </Vue3Marquee>
+
+
+    <div class="relative flex flex-col justify-center pb-10 px-10 items-center overflow-hidden">
+        <div class="main-content px-5 mt-24">
+            <div class="flex flex-col min-w-screen justify-center items-center">
+                <div class="grid md:grid-cols-[1fr_300px] md:gap-24 gap-4">
+                    <div>
+                        <h2 class="text-2xl md:text-3xl mb-5">My name is İrem. Here are some quick facts about me:</h2>
+                        <ul class="list-inside list-['-_']">
+                            <li class="mb-2 text-xl md:text-2xl">I'm mainly a Go and Vue.js/TypeScript developer.</li>
+                            <li class="mb-2 text-xl md:text-2xl">I system-administer an awesome community called <a
+                                    href="https://kernal.eu" class="italic underline">kernal.eu</a>.</li>
+                            <li class="mb-2 text-xl md:text-2xl">I'm a <a class="italic underline"
+                                    href="https://kernal.eu/~siren/pentestplus.pdf">certified</a> penetration tester.
+                            </li>
+                            <li class="mb-2 text-xl md:text-2xl">I'm a free software advocate and a Linux user of ~10
+                                years.
+                                Currently on <a href="https://github.com/sausagenoods/nixconfig"
+                                    class="underline">NixOS</a>.</li>
+                            <li class="mb-2 text-xl md:text-2xl">I'm one of the directors of a <a
+                                    href="https://twed.org" class="italic underline">non-profit</a> in Czech Republic
+                                whose main activity is organizing <a href="https://monerokon.org"
+                                    class="italic underline">Monero Konferenco</a>.</li>
+                            <li class="mb-2 text-xl md:text-2xl">I'm a 4th year informatics student at VILNIUS TECH.
+                            </li>
+                        </ul>
+                    </div>
+                    <img class="irem" src="/irem.webp" />
+                </div>
+            </div>
+
             <div class="mt-10 items-center flex flex-row gap-4">
                 <a href="/#projects"
                     class="text-xl bg-transparent hover:bg-black dark:hover:bg-white text-black dark:text-white font-semibold hover:text-white dark:hover:text-black py-2 px-4 border border-black dark:border-white hover:border-transparent rounded transition-colors">Projects</a>
@@ -69,29 +104,30 @@ onMounted(() => {
 
             <div class="grid md:grid-cols-2 gap-5">
 
-                <ProjectCard title="CNAME.sh" link="https://cname.sh" img="cname-dark.png"
+                <ProjectCard title="CNAME.sh" link="https://cname.sh" img="cname-dark.webp"
                     desc="An upcoming platform that offers Dynamic DNS, DNS load balancing, Geo-routing and health-checks. I made the backend, the frontend and the landing website."
                     :tags='["Go", "Vue.js", "CockroachDB", "RabbitMQ", "TypeScript"]' />
 
-                <ProjectCard title="MoneroPay" link="https://moneropay.eu" img="moneropay.png"
+                <ProjectCard title="MoneroPay" link="https://moneropay.eu" img="moneropay.webp"
                     desc="The most popular payment processor for the Monero cryptocurrency. Written in Go and uses frameworks such as go-chi and zerolog."
                     :tags='["Go", "PostgreSQL", "SQLite"]' />
 
-                <ProjectCard title="The Monero ATM Project" link="https://atm.monero.is" img="monero-atm.png"
+                <ProjectCard title="The Monero ATM Project" link="https://atm.monero.is" img="monero-atm.webp"
                     desc="Open-source cryptocurrency ATM. All component communication happens over MQTT. Frontend is in Vue.js and has i18n support. For security, no wallet or keys are stored locally."
                     :tags='["Go", "MQTT", "IoT", "Vue.js", "TypeScript"]' />
 
-                <ProjectCard title="VPN Application" img="poisonvpn.png"
+                <ProjectCard title="VPN Application" img="poisonvpn.webp"
                     desc="A proprietary VPN application/infrastructure I designed and programmed for a client. It's written in Go and uses xray-core. The backend consists of agents and a management server communicating in a private API secured by self-signed certificates. I used Vue.js for the management panel."
                     :tags='["Go", "Vue.js", "TypeScript"]' />
 
-                <ProjectCard title="Website for Digilol" link="https://www.digilol.net" img="digilolwebsite.png"
+                <ProjectCard title="Website for Digilol" link="https://www.digilol.net" img="digilolwebsite.webp"
                     desc="The website for Digilol that I built using Vitepress with custom components in Vue.js."
                     :tags='["Vue.js", "TypeScript"]' />
             </div>
 
             <p class="text-xl md:text-2xl mt-10 mb-2 text-center">You can find more of my projects and libraries on my
-                <a class="underline" href="https://github.com/sausagenoods">GitHub</a>.</p>
+                <a class="underline" href="https://github.com/sausagenoods">GitHub</a>.
+            </p>
             <p class="text-xl md:text-xl text-center">This website is also <a class="underline"
                     href="https://github.com/sausagenoods/portfolio">open-source</a>.</p>
         </div>
@@ -119,6 +155,12 @@ onMounted(() => {
             <p class="text-2xl md:text-3xl mt-10 text-center">...or email me at siren[at]kernal.eu</p>
         </div>
     </div>
+    <Vue3Marquee class="led-footer overflow-hidden" :clone="true" :gradient="isMobile"
+        :gradient-color="colorMode === 'light' ? [255, 255, 255] : [0, 0, 0]">
+        <span v-for="(word, index) in cyaArray" :key="index">
+            {{ word }}
+        </span>
+    </Vue3Marquee>
 </template>
 
 <style scoped>
@@ -136,5 +178,33 @@ onMounted(() => {
 
 .animate-bounce {
     animation: bounce 2s infinite;
+}
+
+.led {
+    font-size: 5em;
+    font-family: "Micro 5 Charted", sans-serif;
+    color: orangered;
+    text-shadow: rgba(255, 0, 0, 0.9) 0px 0px 39px;
+    transform: rotate(-10deg);
+    top: -1em;
+    right: 1em;
+}
+
+.led-footer {
+    font-size: 5em;
+    font-family: "Micro 5 Charted", sans-serif;
+    color: orangered;
+    text-shadow: rgba(255, 0, 0, 0.9) 0px 0px 39px;
+}
+
+@media (max-width: 768px) {
+    .led {
+        top: 0.5em;
+        right: 0em;
+    }
+}
+
+.irem {
+    border: 5px solid orangered;
 }
 </style>
