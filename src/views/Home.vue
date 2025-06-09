e25d6bf2c6c4cde429c59d6a1ebd377659f379b8
Irem Kuyucu <irem@digilol.net>
2025-06-09 04:00:57 +0300

    Update portfolio, add CVE advisory, MITRE

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,11 @@ import { useWindowSize, useColorMode } from '@vueuse/core'
 import { SunIcon, MoonIcon } from '@heroicons/vue/24/solid'
 
 const posts = [
+    { title: 'DLSEC-2025-1: 3X-UI Update Accepts Forged Server Certificates (CVE-2025-29331)', url: 'https://www.digilol.net/security-advisories/dlsec2025-001.html' },
     { title: 'How Chainalysis Made Their Way into Popular Monero Wallets', url: 'https://www.digilol.net/blog/chainanalysis-malicious-xmr.html' },
-    { title: 'Establishing persistence using extended attributes on Linux', url: 'https://kernal.eu/posts/linux-xattr-persistence' },
+    { title: 'Establishing persistence using extended attributes on Linux (featured on MITRE ATT&CK)', url: 'https://kernal.eu/posts/linux-xattr-persistence' },
     { title: 'Encrypt Communications using Self-signed Certificates in Go', url: 'https://www.digilol.net/blog/self-signed-certs-go.html' },
     { title: 'How to Host a Website on the I2P Anonymous Network', url: 'https://www.digilol.net/blog/i2p-hosting.html' },
-    { title: 'How to Make a Responsive Contact Form Using Vue.js and Go', url: 'https://www.digilol.net/blog/contact-form-in-vuejs.html' },
 ]
 
 const helloArray = ['Hello!\xa0', 'Labas!\xa0', 'Merhaba!\xa0']
@@ -58,8 +58,7 @@ onMounted(() => {
                     <div>
                         <h2 class="text-2xl md:text-3xl mb-5">My name is İrem. Here are some quick facts about me:</h2>
                         <ul class="list-inside list-['-_']">
-                            <li class="mb-2 text-xl md:text-2xl">I'm mainly a Go and Vue.js/TypeScript developer.</li>
-                            <li class="mb-2 text-xl md:text-2xl">I system-administer an awesome community called <a
+                            <li class="mb-2 text-xl md:text-2xl">I system-administer a hacker community called <a
                                     href="https://kernal.eu" class="italic underline">kernal.eu</a>.</li>
                             <li class="mb-2 text-xl md:text-2xl">I'm a certified penetration tester (<a class="italic underline"
                                     href="https://kernal.eu/~siren/pentestplus.pdf">Pentest+</a>) and Kubernetes administrator (<a
@@ -69,19 +68,14 @@ onMounted(() => {
                                 years.
                                 Currently on <a href="https://github.com/sausagenoods/nixconfig"
                                     class="underline">NixOS</a>.</li>
-                            <li class="mb-2 text-xl md:text-2xl">I'm one of the directors of a <a
-                                    href="https://twed.org" class="italic underline">non-profit</a> in Czech Republic
-                                whose main activity is organizing <a href="https://monerokon.org"
-                                    class="italic underline">Monero Konferenco</a>.</li>
-                            <li class="mb-2 text-xl md:text-2xl">I'm a 4th year informatics student at VILNIUS TECH.
-                            </li>
+                            <li class="mb-2 text-xl md:text-2xl">I program a lot in Go and Vue.js/TypeScript these days.</li>
                         </ul>
                     </div>
                     <img class="irem" src="/irem.webp" />
                 </div>
             </div>
 
-            <div class="mt-10 items-center flex flex-row gap-4">
+            <div class="items-center flex flex-row gap-4">
                 <a href="/#projects"
                     class="text-xl bg-transparent hover:bg-black dark:hover:bg-white text-black dark:text-white font-semibold hover:text-white dark:hover:text-black py-2 px-4 border border-black dark:border-white hover:border-transparent rounded transition-colors">Projects</a>
                 <a href="/#blog"
@@ -89,7 +83,7 @@ onMounted(() => {
                 <a href="/cv.pdf"
                     class="text-xl bg-transparent hover:bg-black dark:hover:bg-white text-black dark:text-white font-semibold hover:text-white dark:hover:text-black py-2 px-4 border border-black dark:border-white hover:border-transparent rounded transition-colors">CV</a>
             </div>
-            <div class="flex justify-center mt-10">
+            <div class="flex justify-center mt-16">
                 <a href="#projects" class="text-3xl animate-bounce">
                     ↓
                 </a>
@@ -100,20 +94,23 @@ onMounted(() => {
     <div id="projects" class="relative flex flex-row py-10 lg:px-48 justify-center items-center px-5 overflow-hidden">
         <div>
             <p class="text-4xl md:text-5xl mb-10 text-center">Projects</p>
-            <p class="text-xl md:text-2xl mb-10 text-center">List of projects where I am the sole maintainer or the main
+            <p class="text-xl md:text-2xl mb-10 text-center">Some projects where I am the sole maintainer or the main
                 contributor.</p>
 
             <div class="grid md:grid-cols-2 gap-5">
-
-
-                <ProjectCard title="CNAME.sh" link="https://cname.sh" img="cname-dark.webp"
-                    desc="An upcoming platform that offers Dynamic DNS, DNS load balancing, Geo-routing and health-checks. I made the backend, the frontend and the landing website."
-                    :tags='["Go", "Vue.js", "CockroachDB", "RabbitMQ", "TypeScript"]' />
-                
+		    <ProjectCard title="Plastik3D" link="https://plastik3d.net" img="plastik3d.webp" desc="A system for ordering 3D prints, with automatic print evaluation and cost estimation." :tags='["Go", "Vue.js"]' />
 		<ProjectCard title="The Monero ATM Project" link="https://atm.monero.is" img="monero-atm.webp"
                     desc="Open-source cryptocurrency ATM. All component communication happens over MQTT. Frontend is in Vue.js and has i18n support. For security, no wallet or keys are stored locally."
                     :tags='["Go", "MQTT", "IoT", "Vue.js", "TypeScript"]' />
-                
+
+                <ProjectCard title="MoneroPay" link="https://moneropay.eu" img="moneropay.webp"
+                    desc="The most popular payment processor for the Monero cryptocurrency. Written in Go and uses frameworks such as go-chi and zerolog."
+                    :tags='["Go", "PostgreSQL", "SQLite"]' />
+
+		<ProjectCard title="CNAME.sh" link="https://cname.sh" img="cname-dark.webp"
+                    desc="An upcoming platform that offers Dynamic DNS, DNS load balancing, Geo-routing and health-checks. I made the backend, the frontend and the landing website."
+                    :tags='["Go", "Vue.js", "CockroachDB", "RabbitMQ", "TypeScript"]' />                
+
 		<ProjectCard title="Metronero" link="https://github.com/metronero" video="metronero.webm"
                     desc="Multi-merchant, highly customizable and scalable checkout and PoS system for the Monero cryptocurrency."
                     :tags='["Go", "Vue.js", "PostgreSQL"]' />
@@ -122,17 +119,13 @@ onMounted(() => {
                     desc="The website for Digilol that I built using Vitepress with custom components in Vue.js."
                     :tags='["Vue.js", "TypeScript"]' />
 
+		<!--ProjectCard title="VPN Application" img="poisonvpn.webp"
+                    desc="A proprietary VPN application/infrastructure I designed and programmed for a client. It's written in Go and uses xray-core. The backend consists of agents and a management server communicating in a private API secured by self-signed certificates. I used Vue.js for the management panel."
+                    :tags='["Go", "Vue.js", "TypeScript"]' /-->
+
                 <ProjectCard title="Turkish Formant Speech Synthesizer" link="https://youtu.be/GelelqJ59tU" img="lapic.png"
                     desc="A simple speech synthesizer that uses LPC decoding. It's possible for it to speak other languages in Turkish-ified form with a funny accent. Click the link for a demo of the synthesizer singing Daisy Bell."
                     :tags='["C", "C++"]' />
-
-		<ProjectCard title="VPN Application" img="poisonvpn.webp"
-                    desc="A proprietary VPN application/infrastructure I designed and programmed for a client. It's written in Go and uses xray-core. The backend consists of agents and a management server communicating in a private API secured by self-signed certificates. I used Vue.js for the management panel."
-                    :tags='["Go", "Vue.js", "TypeScript"]' />
-
-                <ProjectCard title="MoneroPay" link="https://moneropay.eu" img="moneropay.webp"
-                    desc="The most popular payment processor for the Monero cryptocurrency. Written in Go and uses frameworks such as go-chi and zerolog."
-                    :tags='["Go", "PostgreSQL", "SQLite"]' />
 
 
             </div>
